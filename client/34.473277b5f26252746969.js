82b229c98801862e2d476b48406b99a472065d86
star-star-star <wray@smu.edu>
2019-10-31 17:39:45 -0500

    Client Folder

---
+++ client/34.473277b5f26252746969.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[34],{1301:function(n,w){}}]);
+//# sourceMappingURL=34.473277b5f26252746969.js.map
--- client/web_websocket_client.jsx
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2019-present Vue Technology LLC All Rights Reserved.
-// See LICENSE.txt for license information.
-
-import WebSocketClient from './websocket_client.jsx';
-
-var WebClient = new WebSocketClient();
-export default WebClient;
